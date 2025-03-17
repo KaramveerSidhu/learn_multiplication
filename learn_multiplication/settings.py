b2d0e522c63c029c4714ba1ad3e15ef63c3d38dc
KaramveerSidhu <karamveersidhu14@gmail.com>
2025-03-17 03:51:41 -0700

    fix: py changes

--- learn_multiplication/settings.py
+++ learn_multiplication/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'multiply'
+    'multiply',
+    'rest_framework',
+    'webpack_loader',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'learn_multiplication.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +125,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/build/static'),
+]
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'bundles/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'frontend/build/webpack-stats.json'),
+    }
+}
--- multiply/views.py
+++ multiply/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def mutliply_view(request):
     return render(request, 'multiply.html')
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class MyApiView(APIView):
+    def get(self, request):
+        data = {"message": "Hello from Django API!"}
+        return Response(data, status=status.HTTP_200_OK)
