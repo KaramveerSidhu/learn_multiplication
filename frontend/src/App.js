6022b7143e6cab4294742bbd3fa0a1c780e75ea4
KaramveerSidhu <karamveersidhu14@gmail.com>
2025-03-20 16:59:23 -0700

    feat: create main game logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,6 @@ import TicTacToe from './components/TicTacToe';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Learn Multiplication
-      </header>
 
       <TicTacToe />
     </div>
--- multiply/urls.py
+++ multiply/urls.py
@@ -5,4 +5,5 @@ from .views import MyApiView
 urlpatterns = [
     # path('', views.mutliply_view, name='multiply'),
     path('multiply/', MyApiView.as_view(), name='multiply-api'),
+    path('generate-questions/', views.generate_questions, name='generate-questions'),
 ]
