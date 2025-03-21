3ffec010a27ee891f51ad153d7b3cc7477843d5e
KaramveerSidhu <karamveersidhu14@gmail.com>
2025-03-21 01:35:28 -0700

    fix: static files fix

--- learn_multiplication/settings.py
+++ learn_multiplication/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -135,6 +135,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 WEBPACK_LOADER = {
     'DEFAULT': {
